7306bbb7f988421e759d24adc351a927b4ffeda4
Agimir <william@mesaindigo.com>
2024-09-03 13:30:14 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -33,19 +33,6 @@ files {
 
 --[[    data files    ]]--
 
--- k4mb1 starter shells (unfurnished)
-data_file 'DLC_ITYP_REQUEST' 'starter_shells_k4mb1.ytyp'
-
--- vehicle data files
-data_file 'HANDLING_FILE' 'stream/**/**/handling.meta'
-data_file 'VEHICLE_METADATA_FILE' 'stream/**/**/vehicles.meta'
-data_file 'VEHICLE_METADATA_FILE' 'stream/**/**/vehiclelayouts.meta'
-data_file 'CARCOLS_FILE' 'stream/**/**/carcols.meta'
-data_file 'VEHICLE_VARIATION_FILE' 'stream/**/**/carvariations.meta'
-data_file 'VEHICLE_LAYOUTS_FILE' 'stream/**/**/dlctext.meta'
-data_file 'VEHICLE_METADATA_FILE' 'stream/**/**/contentunlocks.meta'
-data_file 'AMBIENT_PED_MODEL_SET_FILE' 'stream/**/**/ambientpedmodelsets.meta'
-
 -- props
 data_file 'DLC_ITYP_REQUEST' 'stream/**/**/badges.ytyp'
 data_file 'DLC_ITYP_REQUEST' 'stream/**/**/bzzz_effect_cigarpack.ytyp'
@@ -53,16 +40,7 @@ data_file 'DLC_ITYP_REQUEST' 'stream/**/**/bzzz_food_dessert_a.ytyp'
 data_file 'DLC_ITYP_REQUEST' 'stream/**/**/bzzz_food_icecream_pack.ytyp'
 data_file 'DLC_ITYP_REQUEST' 'stream/**/**/bzzz_food_xmas22.ytyp'
 data_file 'DLC_ITYP_REQUEST' 'stream/**/**/bzzz_foodpack.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/bzzz_plant_coca_a.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/**/**/alca_wallet.ytyp'
 data_file 'DLC_ITYP_REQUEST' 'stream/**/**/dingus.ytyp'
 data_file 'DLC_ITYP_REQUEST' 'stream/**/**/knjgh_pizzas.ytyp'
 
--- weapons
-data_file 'WEAPONCOMPONENTSINFO_FILE' 'stream/**/**/weaponcomponents.meta'
-data_file 'WEAPON_METADATA_FILE' 'stream/**/**/weaponarchetypes.meta'
-data_file 'WEAPON_ANIMATIONS_FILE' 'stream/**/**/weaponanimations.meta'
-data_file 'PED_PERSONALITY_FILE' 'stream/**/**/pedpersonality.meta'
-data_file 'WEAPONINFO_FILE' 'stream/**/**/weapons.meta'
-
--- names
-client_script 'cl_weaponNames.lua'
