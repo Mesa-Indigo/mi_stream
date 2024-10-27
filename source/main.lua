9a4d5a457d0fe74ba04beedc3a3874b3acceed59
Agimir <william@mesaindigo.com>
2024-10-26 19:41:58 -0500

    rem props for housing / planning some vehicles for jobs pack

--- source/main.lua
+++ source/main.lua
@@ -9,10 +9,5 @@ end
 -- SetVehicleName('word', 'word')
 
 CreateThread(function()
-    SetVehicleName('zr', 'Annis ZR-350')
-    SetVehicleName('rhinetaxi', 'Rhinehart Taxi')
-    SetVehicleName('word', 'Vapid Sandstorm D205')
-    SetVehicleName('word', 'Benefactor Imperial')
-    SetVehicleName('word', 'Vapid Riata TX&S')
-    SetVehicleName('trailerflat2', 'Brute Trailer')
+    SetVehicleName('nspeedo', 'Vapid Speedo-Express')
 end)
